951246d5e2c972b11b9e72330d7f6fd0389ad08a
Americo Sierra <eltrogloditakia@gmail.com>
2020-05-10 17:16:32 -0500

    add users

--- microservices/ms-hhrr-users/src/main/java/com/kuroshan/ms/hhrr/users/repositories/UserRepository.java
+++ microservices/ms-hhrr-users/src/main/java/com/kuroshan/ms/hhrr/users/repositories/UserRepository.java
@@ -5,12 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "/users")
+@RepositoryRestResource(path = "users")
 public interface UserRepository extends JpaRepository<User, Long> {
 
   public User findByUsername(String username);
 
-  @Query("select u from User u where u.username = ?")
+  @Query("select u from User u where u.username = ?1")
   public User obtenerPorUsername(String username);
 
 }
---
+++ microservices/ms-hhrr-users/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into users (username, password, email, enabled) values ('jsierra', '123456', 'jsierra@kuroshan.com', true)
+insert into users (username, password, email, enabled) values ('klozano', '123456', 'klozano@kuroshan.com', true)
